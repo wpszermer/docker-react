07431e49a4d13176e95efa2e85a2b4449f6ba2eb
wpszermer <wpszermer@gmail.com>
2019-10-20 13:20:01 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi there and bye!!!
         </p>
         <a
           className="App-link"
